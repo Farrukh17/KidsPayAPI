c0c03b2046cae434a0eb6ab7bae05bbc4a19fb1d
Umar Kholdarbekov <kdbuz.developer@gmail.com>
2019-10-26 10:43:40 +0500

    cheques upload folder path

--- finance/models.py
+++ finance/models.py
@@ -1,6 +1,13 @@
+import datetime
 from django.db import models
 from core.models import Child, App, School
 from django.utils.timezone import localtime
+
+
+def get_cheque_upload_folder(instance, filename):
+    dt = datetime.datetime.today()
+    return 'cheques/{school}/{year}-{month}/{filename}'.format(school=instance.school.name, year=dt.year,
+                                                               month=dt.month, filename=filename)
 
 
 class Transaction(models.Model):
@@ -15,7 +22,7 @@ class Transaction(models.Model):
     terminal = models.CharField(max_length=32, verbose_name='Терминал', blank=True, null=True)
     mfo = models.CharField(max_length=5, verbose_name='МФО банка')
     paymentMethod = models.CharField(choices=PAYMENT_METHODS, default=PAYMENT_METHODS[1][0], max_length=12, verbose_name='Метод оплаты', null=True)
-    cheque = models.FileField(upload_to='cheques', null=True, verbose_name='Чек')
+    cheque = models.FileField(upload_to=get_cheque_upload_folder, null=True, verbose_name='Чек')
     appType = models.ForeignKey(App, on_delete=models.CASCADE, verbose_name='Название приложение', null=True)
     school = models.ForeignKey(School, on_delete=models.CASCADE, related_name='transactions', null=True)
 
@@ -24,7 +31,7 @@ class Transaction(models.Model):
         verbose_name_plural = 'Платежи'
 
     def __str__(self):
-        return '{child} - {amount} UZS - {month}'  .format(child=self.child, amount=self.amount, month=self.paymentTime.strftime('%Y.%m.%d'))
+        return '{child} - {amount} UZS - {month}'.format(child=self.child, amount=self.amount, month=self.paymentTime.strftime('%Y.%m.%d'))
 
     def clean(self):
         self.paymentMethod = self.PAYMENT_METHODS[1][0]
