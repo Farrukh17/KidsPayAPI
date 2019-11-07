4c75c0d5c5b8ec17d45916af11e4bcaf5cc39bb3
Umar Kholdarbekov <kdbuz.developer@gmail.com>
2019-11-08 00:29:43 +0500

    pg backup schedule to 00:00

--- scheduler/backupScheduler.py
+++ scheduler/backupScheduler.py
@@ -4,5 +4,5 @@ from apscheduler.schedulers.background import BackgroundScheduler
 
 def start():
     scheduler = BackgroundScheduler()
-    scheduler.add_job(databaseBackup.backup, 'interval', days=1)  # run every day
+    scheduler.add_job(databaseBackup.backup, 'cron', hour=0)  # run every day at 00:00
     scheduler.start()
