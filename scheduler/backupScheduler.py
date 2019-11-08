ac5731867eb03635b6d76a3d5a15dc0aea9ba6a0
Umar Kholdarbekov <kdbuz.developer@gmail.com>
2019-11-08 14:16:39 +0500

    scheduler fix multiple instances

--- scheduler/backupScheduler.py
+++ scheduler/backupScheduler.py
@@ -4,6 +4,5 @@ from apscheduler.schedulers.background import BackgroundScheduler
 
 def start():
     scheduler = BackgroundScheduler()
-    scheduler.remove_all_jobs()
     scheduler.add_job(databaseBackup.backup, 'cron', hour=0, id='pd_backup', replace_existing=True)  # run every day at 00:00
     scheduler.start()
--- scheduler/paymentScheduler.py
+++ scheduler/paymentScheduler.py
@@ -4,6 +4,5 @@ from apscheduler.schedulers.background import BackgroundScheduler
 
 def start():
     scheduler = BackgroundScheduler()
-    scheduler.remove_all_jobs()
     scheduler.add_job(recalculate.recalculate_schools, 'cron', hour=2, id='auto_calculate', replace_existing=True)  # run every day at 02:00
     scheduler.start()
