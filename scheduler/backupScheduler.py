6ed3f1f97e56f91d3984a6a74a3342aea542d1cd
farrukh <xamidovf@list.ru>
2019-11-09 12:06:15 +0500

    shutdown schedulers

--- scheduler/backupScheduler.py
+++ scheduler/backupScheduler.py
@@ -4,5 +4,6 @@ from apscheduler.schedulers.background import BackgroundScheduler
 
 def start():
     scheduler = BackgroundScheduler()
-    scheduler.add_job(databaseBackup.backup, 'cron', hour=0, id='pd_backup', replace_existing=True)  # run every day at 00:00
-    scheduler.start()
+    scheduler.shutdown(wait=False)
+    # scheduler.add_job(databaseBackup.backup, 'cron', hour=0, id='pd_backup', replace_existing=True)  # run every day at 00:00
+    # scheduler.start()
