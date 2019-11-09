2279ee0609a7d1b5989f790d65e5a7944b3952e6
farrukh <xamidovf@list.ru>
2019-11-09 12:04:31 +0500

    shutdown schedulers add password to backup config

--- scheduler/paymentScheduler.py
+++ scheduler/paymentScheduler.py
@@ -4,5 +4,6 @@ from apscheduler.schedulers.background import BackgroundScheduler
 
 def start():
     scheduler = BackgroundScheduler()
-    scheduler.add_job(recalculate.recalculate_schools, 'cron', hour=2)  # run every day at 02:00
-    scheduler.start()
+    scheduler.shutdown(wait=False)
+    # scheduler.add_job(recalculate.recalculate_schools, 'cron', hour=2)  # run every day at 02:00
+    # scheduler.start()
