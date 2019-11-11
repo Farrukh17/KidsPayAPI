eb20b5e2a17ca59fd12d5e5529843659d85a9739
farrukh <xamidovf@list.ru>
2019-11-11 23:39:07 +0500

    gunicore worker class = gevent

--- config/gunicorn.conf.py
+++ config/gunicorn.conf.py
@@ -2,6 +2,7 @@ import multiprocessing
 
 bind = '127.0.0.1:8000'
 workers = multiprocessing.cpu_count() * 2 + 1
+worker_class = "gevent"
 pid = '/var/www/kidspay/shared/run/gunicorn.pid'
 log_level = 'debug'
 errorlog = '-'
