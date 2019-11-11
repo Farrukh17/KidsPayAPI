2fc4a440aafa7093bd2117a85dd4deb50c8f44c6
farrukh <xamidovf@list.ru>
2019-11-12 00:41:18 +0500

    gunicorn preload_app = True

--- config/gunicorn.conf.py
+++ config/gunicorn.conf.py
@@ -2,7 +2,8 @@ import multiprocessing
 
 bind = '127.0.0.1:8000'
 workers = multiprocessing.cpu_count() * 2 + 1
-worker_class = "gevent"
+# worker_class = "gevent"
+preload_app = True
 pid = '/var/www/kidspay/shared/run/gunicorn.pid'
 log_level = 'debug'
 errorlog = '-'
