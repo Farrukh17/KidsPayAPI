41704c39df1f81c68dc5deadba325d8a03eabbe6
farrukh <xamidovf@list.ru>
2019-11-12 22:54:47 +0500

    backup scheduler

--- scheduler/backupScheduler.py
+++ scheduler/backupScheduler.py
@@ -4,5 +4,5 @@ from apscheduler.schedulers.background import BackgroundScheduler
 
 def start():
     scheduler = BackgroundScheduler()
-    scheduler.add_job(databaseBackup.backup, 'cron', hour=22, minute=50,id='pd_backup', replace_existing=True)  # run every day at 00:00
+    scheduler.add_job(databaseBackup.backup, 'cron', hour=22, minute=57,id='pd_backup', replace_existing=True)  # run every day at 00:00
     scheduler.start()
